50f6842c62041d68a5d9f705aaae7ea0cac3e54d
Francesco Petrini <francescogpetrini@gmail.com>
2023-02-08 19:30:35 -0800

    Adding Free assets and demo level

---
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/CameraFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Cainos.PixelArtTopDown_Basic
+{
+    //let camera follow target
+    public class CameraFollow : MonoBehaviour
+    {
+        public Transform target;
+        public float lerpSpeed = 1.0f;
+
+        private Vector3 offset;
+
+        private Vector3 targetPos;
+
+        private void Start()
+        {
+            if (target == null) return;
+
+            offset = transform.position - target.position;
+        }
+
+        private void Update()
+        {
+            if (target == null) {
+                Console.Write("target is null");
+                return;
+            }
+
+            targetPos = target.position + offset;
+            transform.position = Vector3.Lerp(transform.position, targetPos, lerpSpeed * Time.deltaTime);
+        }
+
+    }
+}
