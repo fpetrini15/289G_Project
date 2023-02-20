4c2157ca2b8ebbae5fa9b16108cb06ce05bdc652
Francesco Petrini <francescogpetrini@gmail.com>
2023-02-19 20:44:25 -0800

    Patrolling AIs and more generic Patrol script

--- Assets/Cainos/Pixel Art Top Down - Basic/Script/GuardPatrol.cs
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/GuardPatrol.cs
@@ -5,26 +5,32 @@ using UnityEngine;
 public class GuardPatrol : MonoBehaviour
 {
     public Transform[] patrolPoints;
-    public int patrolIndex = 0;
+    public int patrolIndex;
+    public int previousPatrolIndex;
     public float speed;
+    public string defaultDirection;
 
     private Animator animator;
     private float waitTime;
     public float startWaitTime;
-    private bool patrolPointReached;
+    private bool patrolPointReached; 
 
     private void Start()
     {
+        patrolIndex = 0;
+        previousPatrolIndex = 0;
         patrolPointReached = false;
         waitTime = startWaitTime;
         animator = GetComponent<Animator>();
-        ContinuePatrol();
+        StartPatrol();
+
     }
 
 
     private void Update()
     {
         if(Vector2.Distance(this.transform.position, patrolPoints[patrolIndex].transform.position) < 0.35) {
+            previousPatrolIndex = patrolIndex;
             patrolIndex = (patrolIndex+1)%patrolPoints.Length;
             GetComponent<Rigidbody2D>().velocity = Vector2.zero;
             animator.SetBool("IsMoving", false);
@@ -42,15 +48,15 @@ public class GuardPatrol : MonoBehaviour
         }
     }
 
-    private void ContinuePatrol() {
+    private void StartPatrol() {
         Vector2 dir = Vector2.zero;
-        if(patrolIndex == 0) { 
+        if(defaultDirection == "Left") { 
             dir.x = -1;
             animator.SetInteger("Direction", 3); /* Animate Left */
-        } else if(patrolIndex == 1) { 
+        } else if(defaultDirection == "Up") { 
             dir.y = 1;
             animator.SetInteger("Direction", 1); /* Animate Up */
-        } else if(patrolIndex == 2) { 
+        } else if(defaultDirection == "Right") { 
             dir.x = 1;
             animator.SetInteger("Direction", 2); /* Animate Right */
         } else { 
@@ -62,5 +68,36 @@ public class GuardPatrol : MonoBehaviour
 
         GetComponent<Rigidbody2D>().velocity = speed * dir;
     }
+
+    private void ContinuePatrol() {
+        Vector2 dir = Vector2.zero;
+        Vector2 nextPatrolPoint = patrolPoints[patrolIndex].transform.position;
+        Vector2 currentPatrolPoint = patrolPoints[previousPatrolIndex].transform.position;
+        Vector2 normalizedDirection = nextPatrolPoint - currentPatrolPoint;
+        
+        if(normalizedDirection[0] != 0.0) { /* Move along X-axis */
+            if(normalizedDirection[0] > 0) {
+                dir.x = 1;
+                animator.SetInteger("Direction", 2); /* Animate Right */
+            } else {
+                dir.x = -1;
+                animator.SetInteger("Direction", 3); /* Animate Left */
+            }
+        }
+        else {
+            if(normalizedDirection[1] > 0) { /* Move along Y-axis */
+                dir.y = 1;
+                animator.SetInteger("Direction", 1); /* Animate Up */
+            } else {
+                dir.y = -1;
+                animator.SetInteger("Direction", 0); /* Animate Down */
+            }
+        }
+        dir.Normalize();
+        animator.SetBool("IsMoving", dir.magnitude > 0);
+
+        GetComponent<Rigidbody2D>().velocity = speed * dir;
+    }
+
 }
 
