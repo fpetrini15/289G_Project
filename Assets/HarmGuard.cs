066002103400439597195cc845732bdfc2b8124b
Payal Kulkarni <payal.k03@gmail.com>
2023-02-26 18:58:21 -0800

    Injure guard with hammer on collision

---
+++ Assets/HarmGuard.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HarmGuard : MonoBehaviour
+{
+
+    public GameObject blood, guard, fov;
+    public bool playerIsClose;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerIsClose){
+            Debug.Log("Colided");
+            GameObject bloodIns = Instantiate(blood, transform.position, Quaternion.identity);
+            Destroy (guard);
+            Destroy (fov);
+        }
+
+    }
+
+    void OnTriggerEnter2D (Collider2D other){
+        if(other.CompareTag("Guard")){
+            playerIsClose = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Guard"))
+        {
+            playerIsClose = false;
+        }
+    }
+
+
+}
