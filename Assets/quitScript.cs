3f5da9bfd551a5d5b0c1e7dba6f4e74c3b5166eb
Payal Kulkarni <payal.k03@gmail.com>
2023-03-16 17:07:22 -0700

    Added Quit Button

---
+++ Assets/quitScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quitScript : MonoBehaviour
+{
+  public void QuitGame(){
+    Application.Quit();
+  }
+}
